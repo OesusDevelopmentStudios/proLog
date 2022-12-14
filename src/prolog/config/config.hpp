eaf63b4167e4a953e1a7909df9d18d45b23c99b5
Adam Krizar <48439304+ApocalypseArisen@users.noreply.github.com>
2022-12-14 17:24:58 +0100

    rework file organization + create log object

--- src/prolog/config/config.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-namespace prolog::config
-{
-    static std::string timeFormat {"%Y-%m-%d %Z %H:%M:%S:"};
-
-}  // namespace prolog::config
---
+++ src/prolog/private/config/config.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+
+namespace prolog::config
+{
+    static std::string dateFormat {"%Y-%m-%d %Z %H:%M:%S:"};
+    static std::string zoneFomrat {};
+
+}  // namespace prolog::config
